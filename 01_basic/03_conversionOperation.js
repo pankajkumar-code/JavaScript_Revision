4e30eb5ffbfb64c0f9fff835eba4d2837087e92a
Pankaj kumar <154460162+pnkajkumar1504@users.noreply.github.com>
2025-12-28 17:54:35 +0000

    Conversion Of DataTypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,38 @@
+// let score = "abc58";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log( typeof valueInNumber);
+// console.log(valueInNumber);  // Nan => Not A Number 
+
+// "58" => 58
+//"58abc" => NaN
+//"abc" => Nan
+//true =>1
+//false => 0
+// null => 0(Number)
+// undefined => NaN
+
+
+
+//let isLoggedIn = 1;
+//let booleanIsLiggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLiggedIn);
+
+// 1=>true , 
+// 0=>False,
+// "" => false
+// "Pankaj" => true
+
+
+
+
+let someNumber= 58;
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
