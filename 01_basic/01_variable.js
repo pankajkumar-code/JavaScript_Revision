069988c5b16373afb2f9002c85dd86088353f0ff
Pankaj kumar <154460162+pnkajkumar1504@users.noreply.github.com>
2025-12-28 14:21:44 +0000

    Variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,19 @@
+
+const accountId=1504;
+let accountEmail= "pankaj@google.com";
+ var accountPassword = 2003;
+ accountCity= "Muzffarpur";
+ let accountState;
+//  accountId=123;  Can`t Modified 
+
+accountEmail="prem@google.com";
+accountPassword= 2000;
+accountCity="Patna";
+//console.log(accountId);
+//console.log([accountId,accountEmail, accountPassword, accountCity]);
+console.table([accountId,accountEmail, accountPassword, accountCity,accountState]);
+
+ 
+ /*
+ Prefere no use var an variable , because issue in block scope and functional scope 
+ */
