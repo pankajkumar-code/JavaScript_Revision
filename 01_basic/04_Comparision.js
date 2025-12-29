13f52113fba17038a87579d6705ea54c47357f1f
Pankaj kumar <154460162+pnkajkumar1504@users.noreply.github.com>
2025-12-29 02:48:42 +0000

    Comparison

---
+++ 01_basic/04_Comparision.js
@@ -0,0 +1,37 @@
+// console.log(2<1);
+// console.log(2>1);
+// console.log(2==2);
+// console.log(2!=2);
+// console.log(2>=2);
+
+//console.log("2">1);
+//console.log("02">1);
+
+//console.log(null>0);  // false
+//console.log(null == 0);  // false 
+//console.log(null>=0);  // True
+
+//console.log(undefined==0); //false
+//console.log(undefined>=0);//false 
+//console.log(undefined<=0);// false 
+//console.log(undefined=0);// 0
+
+
+/**Strict Check => Comparision With Dta type**/
+console.log("2"===2);  //false 
+console.log("2"==2); // true , because it chek inly number not datatype 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
