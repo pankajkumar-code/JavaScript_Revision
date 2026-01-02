bec9f13ce08815f55eb28d71f051f51d2afbb8fd
Pankaj kumar <154460162+pankajkumar-code@users.noreply.github.com>
2026-01-02 19:06:32 +0000

    Date And Time

---
+++ 01_basic/07_DatesInJs.js
@@ -0,0 +1,21 @@
+const myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toDateString());
+// console.log(myDate.toString());
+
+
+// Date is object in JavaScript.
+let myCreateddate= new Date(2003,3,15);
+// console.log(myCreateddate);
+// console.log(myCreateddate.toDateString());
+
+let myTimeStap= Date.now();
+console.log(Math.round(Date.now()));
+console.log(Math.round(Date.now()/1000));
+
+
+
+
+
+
+ 
