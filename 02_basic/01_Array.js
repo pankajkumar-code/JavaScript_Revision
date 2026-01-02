45cf5bc777ba9bf4dfa7e3ec2b7c2235ecca4c54
Pankaj kumar <154460162+pankajkumar-code@users.noreply.github.com>
2026-01-02 20:02:40 +0000

    Array

---
+++ 02_basic/01_Array.js
@@ -0,0 +1,37 @@
+let myArray=[0,1,2,3,4,5];
+//console.log(myArray);
+
+let anotherArr=new Array(11,22,33,44,55)
+//console.log(anotherArr);
+
+
+//     Array Methods
+
+// anotherArr.push(66);
+// console.log(anotherArr);
+// anotherArr.pop()  // pop last elementr without  any input 
+// console.log(anotherArr);
+// anotherArr.unshift(0)  // Add number on  Start index   not recomemded 
+// console.log(anotherArr);
+// anotherArr.shift()
+// console.log(anotherArr);
+// console.log(anotherArr.includes(33));
+// console.log(anotherArr.indexOf(44));
+
+// const newArry=anotherArr.join()
+// console.log(anotherArr);
+// console.log(newArry);
+// console.log(typeof newArry);
+
+const myn1= myArray.slice(1,3)
+ console.log(myn1);
+ console.log("B", myArray);
+
+
+const myn2= myArray.splice(1,3)
+console.log(myn2);
+console.log("B" ,myArray);
+
+
+
+
